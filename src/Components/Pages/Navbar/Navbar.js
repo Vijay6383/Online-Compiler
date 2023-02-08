b597dbd7fb0f36ff3bd814761743e88c285aa974
onam kumar verma <onam.kumar@quencil.in>
2023-02-08 14:26:15 +0530

    homepage-onam-kumar

--- src/Components/Pages/Navbar/Navbar.js
+++ src/Components/Pages/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Navbar.css';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.png'
 
@@ -15,30 +16,50 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar shadow-md text-black text-sm">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        {menuItem}
-                    </ul>
-                </div>
-                <img src={logo} alt="" className='w-12' />
-            </div>
-            <div className="navbar-center hidden lg:flex">
+            <div className="navbar shadow-md text-black text-sm">
+              <div className="navbar-start">
+                <img src={logo} alt="brand-logo" className="w-12 image-position " />
+              </div>
+              <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">{menuItem}</ul>
+              </div>
+              <div className="navbar-end">
                 <ul className="menu menu-horizontal px-1">
-                    {menuItem}
+                  <li>
+                    <Link to="/login">LOGIN</Link>
+                  </li>
+                  <li>
+                    <Link to="/signup">SIGNUP</Link>
+                  </li>
                 </ul>
-            </div>
-            <div className="navbar-end">
-                <ul className="menu menu-horizontal px-1">
-                    <li><Link to='/login'>LOGIN</Link></li>
-                    <li><Link to='/signup'>SIGNUP</Link></li>
+              </div>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-outline  lg:hidden">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />
+                  </svg>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                >
+                  {menuItem}
                 </ul>
+              </div>
             </div>
-        </div>
+          
+          
     );
 };
 
